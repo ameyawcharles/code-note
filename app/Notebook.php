5e4a1f38cd1d05d1f586de6997859e22164cf2af
Charles-Ameyaw <52047146+Charles-Ameyaw@users.noreply.github.com>
2020-05-29 20:06:32 +1200

    Initial Commit - Added .editorconfig file

---
+++ app/Notebook.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Notebook extends Model
+{
+    protected $fillable = ['title'];
+
+    public function notes(){
+    	return $this->hasMany('App\Note');
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
